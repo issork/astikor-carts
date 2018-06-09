76010e51408a3ec37b54b840c89e250b2ca188cf
MennoMax <MennoMax@outlook.de>
2018-06-09 02:28:29 +0200

    updated version

--- src/main/java/astikoor/Astikoor.java
+++ src/main/java/astikoor/Astikoor.java
@@ -19,7 +19,7 @@ import net.minecraftforge.fml.common.network.NetworkRegistry;
 public class Astikoor
 {
     public static final String MODID = "astikoor";
-    public static final String VERSION = "1.1.0";
+    public static final String VERSION = "1.1.1";
 
     @SidedProxy(clientSide = "astikoor.proxy.ClientProxy", serverSide = "astikoor.proxy.ServerProxy")
     public static IProxy proxy;
