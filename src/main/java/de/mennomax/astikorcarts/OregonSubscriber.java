c4ab74768783267886ccc9ccef19da6ef5c5f948
Paul Fulham <paul.fulham0@gmail.com>
2020-01-30 02:09:13 -0800

    Reorganize oregon subscriber logic

--- src/main/java/de/mennomax/astikorcarts/OregonSubscriber.java
+++ src/main/java/de/mennomax/astikorcarts/OregonSubscriber.java
@@ -18,14 +18,19 @@ import net.minecraft.util.text.TextFormatting;
 import net.minecraftforge.client.event.GuiScreenEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.ObfuscationReflectionHelper;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.lwjgl.glfw.GLFW;
 
 import java.lang.reflect.Field;
+import java.util.Optional;
 import java.util.Random;
 import java.util.concurrent.BlockingDeque;
 import java.util.concurrent.LinkedBlockingDeque;
 
 public final class OregonSubscriber {
+    private static final Logger LOGGER = LogManager.getLogger();
+
     private State state = new IdleState();
 
     @SubscribeEvent
@@ -33,38 +38,19 @@ public final class OregonSubscriber {
         final Minecraft mc = Minecraft.getInstance();
         final Screen screen = event.getGui();
         if (screen instanceof StatsScreen && (event.getKeyCode() == GLFW.GLFW_KEY_ENTER || event.getKeyCode() == GLFW.GLFW_KEY_KP_ENTER) && mc.player != null) {
-            final AbstractList<?> list = ObfuscationReflectionHelper.getPrivateValue(StatsScreen.class, (StatsScreen) screen, "field_146550_h");
-            if (list != null) {
-                final Class<?> classCustomStatsList$Entry;
-                try {
-                    classCustomStatsList$Entry = Class.forName("net.minecraft.client.gui.screen.StatsScreen$CustomStatsList$Entry");
-                } catch (final ClassNotFoundException e) {
-                    return;
+            this.getSelectedStat((StatsScreen) screen).ifPresent(stat -> {
+                if (AstikorStats.CART_ONE_CM.equals(stat.getValue()) && mc.player.getStats().getValue(stat) > 2040 * 100) {
+                    final PlayerIO io = new PlayerIO();
+                    final Oregon oregon = new Oregon(io, new Random());
+                    this.setState(new ActiveState(new Thread(() -> {
+                        try {
+                            oregon.run();
+                        } finally {
+                            mc.enqueue(() -> this.setState(new IdleState()));
+                        }
+                    }, "Oregon Trail 1978"), io));
                 }
-                final AbstractList.AbstractListEntry<?> entry = list.getSelected();
-                if (classCustomStatsList$Entry.isInstance(entry)) {
-                    final Stat<?> stat;
-                    try {
-                        final String statFieldName = ObfuscationReflectionHelper.remapName(INameMappingService.Domain.FIELD, "field_214405_b");
-                        final Field f = classCustomStatsList$Entry.getDeclaredField(statFieldName);
-                        f.setAccessible(true);
-                        stat = (Stat<?>) f.get(entry);
-                    } catch (final NoSuchFieldException | IllegalAccessException e) {
-                        return;
-                    }
-                    if (AstikorStats.CART_ONE_CM.equals(stat.getValue()) && mc.player.getStats().getValue(stat) > 2040 * 100) {
-                        final PlayerIO io = new PlayerIO();
-                        final Oregon oregon = new Oregon(io, new Random());
-                        this.setState(new ActiveState(new Thread(() -> {
-                            try {
-                                oregon.run();
-                            } finally {
-                                mc.enqueue(() -> this.setState(new IdleState()));
-                            }
-                        }, "Oregon Trail 1978"), io));
-                    }
-                }
-            }
+            });
         }
         if (screen instanceof ChatScreen && (event.getKeyCode() == GLFW.GLFW_KEY_ENTER || event.getKeyCode() == GLFW.GLFW_KEY_KP_ENTER)) {
             screen.children().stream()
@@ -76,6 +62,35 @@ public final class OregonSubscriber {
                     }
                 });
         }
+    }
+
+    private Optional<Stat<?>> getSelectedStat(final StatsScreen screen) {
+        final AbstractList<?> list = screen.func_213116_d();
+        if (list == null) {
+            return Optional.empty();
+        }
+        final Class<?> classCustomStatsList$Entry;
+        try {
+            classCustomStatsList$Entry = Class.forName("net.minecraft.client.gui.screen.StatsScreen$CustomStatsList$Entry");
+        } catch (final ClassNotFoundException e) {
+            LOGGER.error("Unable to lookup custom stat entry class", e);
+            return Optional.empty();
+        }
+        final AbstractList.AbstractListEntry<?> entry = list.getSelected();
+        if (!classCustomStatsList$Entry.isInstance(entry)) {
+            return Optional.empty();
+        }
+        final String statFieldName = ObfuscationReflectionHelper.remapName(INameMappingService.Domain.FIELD, "field_214405_b");
+        final Stat<?> stat;
+        try {
+            final Field fieldStat = classCustomStatsList$Entry.getDeclaredField(statFieldName);
+            fieldStat.setAccessible(true);
+            stat = (Stat<?>) fieldStat.get(entry);
+        } catch (final NoSuchFieldException | IllegalAccessException e) {
+            LOGGER.error("Unable to retrieve stat from entry {}", entry.getClass(), e);
+            return Optional.empty();
+        }
+        return Optional.of(stat);
     }
 
     private void setState(final State newState) {
