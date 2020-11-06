5e28da3ddcf0d5acc77e7d2f74e7c3e09bc203a9
Paul Fulham <paul.fulham0@gmail.com>
2020-11-06 01:53:18 -0800

    Simply supply render

--- src/main/java/de/mennomax/astikorcarts/client/renderer/entity/SupplyCartRenderer.java
+++ src/main/java/de/mennomax/astikorcarts/client/renderer/entity/SupplyCartRenderer.java
@@ -31,31 +31,34 @@ public final class SupplyCartRenderer extends DrawnRenderer<SupplyCartEntity, Su
     protected void renderContents(final SupplyCartEntity entity, final float delta, final MatrixStack stack, final IRenderTypeBuffer source, final int packedLight) {
         super.renderContents(entity, delta, stack, source, packedLight);
         final NonNullList<ItemStack> cargo = entity.getCargo();
+        stack.push();
+        this.model.getBody().translateRotate(stack);
         for (int i = 0; i < cargo.size(); i++) {
             final ItemStack itemStack = cargo.get(i);
             if (itemStack.isEmpty()) continue;
             final int ix = i % 2, iz = i / 2;
             if (i < cargo.size() - 2 && cargo.get(i + 2).getItem().isIn(ItemTags.BEDS)) continue;
             if (i >= 2 && cargo.get(i - 2).getItem().isIn(ItemTags.BEDS)) continue;
-            this.attach(this.model.getBody(), this.model.getBody(), s -> {
-                final double x = (ix - 0.5D) * 11.0D / 16.0D;
-                final double z = (iz * 11.0D - 9.0D) / 16.0D;
-                final ItemRenderer renderer = Minecraft.getInstance().getItemRenderer();
-                final IBakedModel model = renderer.getItemModelWithOverrides(itemStack, null, null);
-                if (model.isGui3d()) {
-                    s.translate(x, -0.46D, z);
-                    s.scale(0.65F, 0.65F, 0.65F);
-                    s.rotate(Vector3f.ZP.rotationDegrees(180.0F));
-                    if (iz < 1 && itemStack.getItem().isIn(ItemTags.BEDS)) s.translate(0.0D, 0.0D, 1.0D);
-                } else {
-                    s.translate(x, -0.15D + ((ix + iz) % 2 == 0 ? 0.0D : 1.0e-4D), z);
-                    s.scale(0.7F, 0.7F, 0.7F);
-                    s.rotate(Vector3f.YP.rotationDegrees(Objects.hashCode(itemStack.getItem().getRegistryName()) / 65536.0F));
-                    s.rotate(Vector3f.XP.rotationDegrees(-90.0F));
-                }
-                renderer.renderItem(itemStack, ItemCameraTransforms.TransformType.NONE, false, s, source, packedLight, OverlayTexture.NO_OVERLAY, model);
-            }, stack);
+            final double x = (ix - 0.5D) * 11.0D / 16.0D;
+            final double z = (iz * 11.0D - 9.0D) / 16.0D;
+            final ItemRenderer renderer = Minecraft.getInstance().getItemRenderer();
+            final IBakedModel model = renderer.getItemModelWithOverrides(itemStack, null, null);
+            stack.push();
+            if (model.isGui3d()) {
+                stack.translate(x, -0.46D, z);
+                stack.scale(0.65F, 0.65F, 0.65F);
+                stack.rotate(Vector3f.ZP.rotationDegrees(180.0F));
+                if (iz < 1 && itemStack.getItem().isIn(ItemTags.BEDS)) stack.translate(0.0D, 0.0D, 1.0D);
+            } else {
+                stack.translate(x, -0.15D + ((ix + iz) % 2 == 0 ? 0.0D : 1.0e-4D), z);
+                stack.scale(0.7F, 0.7F, 0.7F);
+                stack.rotate(Vector3f.YP.rotationDegrees(Objects.hashCode(itemStack.getItem().getRegistryName()) / 65536.0F));
+                stack.rotate(Vector3f.XP.rotationDegrees(-90.0F));
+            }
+            renderer.renderItem(itemStack, ItemCameraTransforms.TransformType.NONE, false, stack, source, packedLight, OverlayTexture.NO_OVERLAY, model);
+            stack.pop();
         }
+        stack.pop();
     }
 
     @Override
